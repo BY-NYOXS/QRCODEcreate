f91df159fa0462a55bd5913835ddc4874aeac41a
nyoxs <164644397+BY-NYOXS@users.noreply.github.com>
2024-05-03 20:45:41 +0300

    Update QRCODEolusturucu.py

--- QRCODEolusturucu.py
+++ QRCODEolusturucu.py
@@ -4,7 +4,7 @@ import qrcode
 
 arayuz = tk.Tk()
 arayuz.title("qrcode")
-arayuz.geometry("350x200")
+arayuz.geometry("350x100")
 
 url=tk.Label(text="QRCODE URL:")
 url.place(x=15, y=15)
@@ -22,9 +22,7 @@ yol_grs=tk.Entry(textvariable=x)
 yol_grs.place(x=98, y=40)
 
 #button
-def olusturma():
-    import qrcode
-    
+def olusturma():    
     qrsave=x.get()
     urlQR=y.get()
     qrURL25= qrcode.make(urlQR)
@@ -34,11 +32,9 @@ def olusturma():
 ols=tk.Button(text="OLUSTUR",command=olusturma)
 ols.place(x=250, y=20)
 
-acıklama = tk.Label(text="QR kodlarını kullanarak iletişim\nbilgilerini paylaşmak artık çok kolay!\nQR kodu oluşturucu uygulamamızla, bir URL veya\nmetin girin, dosya adını\nseçin ve QR kodunu oluşturun.\nHemen deneyin ve bilgilerinizi\npaylaşmayı daha hızlı hale getirin!")
-acıklama.place(x=30, y=70)
 
 developer = tk.Label(text="BY NYOXS")
-developer.place(x=280, y=170)
+developer.place(x=280, y=80)
 
 arayuz.mainloop()
 
